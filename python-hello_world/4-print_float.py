d4381b511b4091a349e22f55564f6480ab493872
rcyubahiro <r.cyubahiro@alustudent.com>
2025-02-20 14:26:58 -0800

    task done

--- python-hello_world/4-print_float.py
+++ python-hello_world/4-print_float.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 number = 3.14159
-print(f"Float: {number:.2f}"))
+
+if not isinstance(number, (int, float)):
+    raise ValueError("Unknown format code 'f' for object of type 'str'")
+
+print(f"Float: {number:.2f}")
