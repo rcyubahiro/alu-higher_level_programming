3f315e881137034199491e58767441bbac2845d7
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-04 16:28:27 -0800

    adv task done

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
 def safe_function(fct, *args):
-try:
-return fct(*args)
-except (ZeroDivisionError, ValueError, TypeError, IndexError) as error:
-print("Exception: {}".format(error), file=sys.stderr)
-return None
+    try:
+        return fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        return None
