e0012caf5aef41d6c77687f0d6a9868602f8a2aa
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-07 09:33:21 -0800

    done

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,12 +11,10 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate a parameter as an integer.
-        
-        Args:
+         Args:
             name (str): The name of the parameter.
             value (int): The parameter to validate.
-            
-        Raises:
+             Raises:
             TypeError: If value is not an integer.
             ValueError: If value is <= 0.
         """
