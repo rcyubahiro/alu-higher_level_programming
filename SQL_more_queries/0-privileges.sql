6398a63550dcdb99388c096b9c0c25b499555dba
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 12:46:14 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,23 +1,4 @@
--- Ensure a clean start
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-
--- Case 1: Verify users don't exist
-SELECT user FROM mysql.user WHERE user IN ('user_0d_1', 'user_0d_2');
-
--- Case 2: Create user_0d_1 with root access
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'password1';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Case 3: Create user_0d_2 with root access
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password2';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-
--- Case 4: Modify user_0d_2 to have only SELECT & INSERT on user_2_db
-CREATE DATABASE IF NOT EXISTS user_2_db;
-REVOKE ALL PRIVILEGES ON *.* FROM 'user_0d_2'@'localhost';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Verify grants
+-- lists privileges of the MySQL users
+REVOKE AUDIT_ABORT_EXEMPT, FIREWALL_EXEMPT, AUTHENTICATION_POLICY_ADMIN, GROUP_REPLICATION_STREAM, PASSWORDLESS_USER_ADMIN, SENSITIVE_VARIABLES_OBSERVER, TELEMETRY_LOG_ADMIN ON . FROM 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
