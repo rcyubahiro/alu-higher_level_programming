a81bec72fb4495ad068a3cfd054d9476e42d79e5
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 05:57:53 -0700

    done

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Function that inserts line of text to file containing a specific string."""
 
+
 def append_after(filename="", search_string="", new_string=""):
     '''Module Search and update'''
     with open(filename, 'r') as f:
-        lines = f.readlines()
+    lines = f.readlines()
     
     with open(filename, 'w') as f:
         for line in lines:
