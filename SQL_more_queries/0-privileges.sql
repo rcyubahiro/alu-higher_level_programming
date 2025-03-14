2588fe92ad0c479dd66cbba663c56d756e56f516
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 09:17:43 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,21 +1,3 @@
--- Check if user_0d_1 exists
-SELECT COUNT(*) INTO @user_0d_1_exists FROM mysql.user WHERE user='user_0d_1' AND host='localhost';
-SELECT 'user_0d_1_exists', @user_0d_1_exists;
-
--- If user_0d_1 exists, show grants
-IF @user_0d_1_exists THEN
-    SHOW GRANTS FOR 'user_0d_1'@'localhost';
-ELSE
-    SELECT 'There is no such grant defined for user ''user_0d_1'' on host ''localhost''';
-END IF;
-
--- Check if user_0d_2 exists
-SELECT COUNT(*) INTO @user_0d_2_exists FROM mysql.user WHERE user='user_0d_2' AND host='localhost';
-SELECT 'user_0d_2_exists', @user_0d_2_exists;
-
--- If user_0d_2 exists, show grants
-IF @user_0d_2_exists THEN
-    SHOW GRANTS FOR 'user_0d_2'@'localhost';
-ELSE
-    SELECT 'There is no such grant defined for user ''user_0d_2'' on host ''localhost''';
-END IF;
+-- Lists all privileges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
