111578ce0fca940133ac51bdf30bda82c9c7e917
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 05:59:47 -0700

    done

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -5,7 +5,7 @@
 def append_after(filename="", search_string="", new_string=""):
     '''Module Search and update'''
     with open(filename, 'r') as f:
-    lines = f.readlines()
+         lines = f.readlines()
     
     with open(filename, 'w') as f:
         for line in lines:
