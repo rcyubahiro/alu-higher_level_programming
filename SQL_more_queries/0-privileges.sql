f6e496549d3fc65bf87472bd5bb9670219c26150
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 09:00:44 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,22 +1,25 @@
--- File: main_0_0.sql
--- Attempt to create users and handle the grant error
+-- Check if user_0d_1 exists
+SELECT EXISTS(
+    SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'
+) AS user_0d_1_exists;
+
+-- Check if user_0d_2 exists
+SELECT EXISTS(
+    SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'
+) AS user_0d_2_exists;
+
+-- Create user_0d_1 if not exists and grant privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
--- Expect error message if GRANT is not allowed
--- Expected: There is no such grant defined for user 'user_0d_1' on host 'localhost'
-
--- File: main_2_0.sql
--- Grant full privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
--- Grant full privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-
--- Attempt to revoke privileges to test error 1269
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
--- Expected error: Can't revoke all privileges for one or more of the requested users
-
--- File: main_3_0.sql
--- Grant specific privileges to users on user_2_db
+-- Create user_0d_2 if not exists and grant SELECT, INSERT privileges
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
 GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
--- Grant root privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Display the grants for both users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+-- Attempt to revoke all privileges (This will trigger error if not possible)
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_2'@'localhost';
