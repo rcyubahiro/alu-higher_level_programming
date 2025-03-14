26f01ac99b6a414d699bbb1ef909ee8c2c8e37a9
rcyubahiro <r.cyubahiro@alustudent.com>
2025-03-14 09:12:05 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,25 +1,14 @@
--- Check if user_0d_1 exists
-SELECT EXISTS(
-    SELECT 1 FROM mysql.user WHERE user = 'user_0d_1' AND host = 'localhost'
-) AS user_0d_1_exists;
+-- Ensure user_0d_1 exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 
--- Check if user_0d_2 exists
-SELECT EXISTS(
-    SELECT 1 FROM mysql.user WHERE user = 'user_0d_2' AND host = 'localhost'
-) AS user_0d_2_exists;
+-- Ensure user_0d_2 exists
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
--- Create user_0d_1 if not exists and grant privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- Grant some privileges to user_0d_2 (if necessary)
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 
--- Create user_0d_2 if not exists and grant SELECT, INSERT privileges
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
-GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
-
--- Display the grants for both users
+-- Display privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
 
--- Attempt to revoke all privileges (This will trigger error if not possible)
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_1'@'localhost';
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'user_0d_2'@'localhost';
+-- Display privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
