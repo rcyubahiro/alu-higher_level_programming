dc65fffd1d459eb4a8455965447372977adb0f10
rcyubahiro <r.cyubahiro@alustudent.com>
2025-04-05 12:29:14 -0700

    done

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""A script that fetches https://intranet.hbtn.io/status with headers."""
+"""
+Script that fetches https://alu-intranet.hbtn.io/status
+"""
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
+    url = "https://alu-intranet.hbtn.io/status"
+    
+    # Using with statement to open the URL
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-    url = 'https://intranet.hbtn.io/status'
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
-    with urllib.request.urlopen(req) as resp:
-        content = resp.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
