486b5d501c43ad8095f9ed88b9363dc715cb2d23
rcyubahiro <r.cyubahiro@alustudent.com>
2025-04-05 12:26:37 -0700

    done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alu-intranet.hbtn.io/status
+Script that fetches http://0.0.0.0:5050/status
 """
 import requests
 
 if __name__ == '__main__':
-    url = "https://alu-intranet.hbtn.io/status"
+    url = "http://0.0.0.0:5050/status"  # Change the URL to the required one
     response = requests.get(url)
     text = response.text
 
