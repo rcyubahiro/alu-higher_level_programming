af222502d13edba6ecce7d04fba80dceb6322857
rcyubahiro <r.cyubahiro@alustudent.com>
2025-04-05 12:23:08 -0700

    done

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
+Script that fetches https://alu-intranet.hbtn.io/status
 """
-
 import requests
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    r = requests.get(url, headers=headers)
-    text = r.text
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+    text = response.text
+
     print("Body response:")
     print("\t- type: {}".format(type(text)))
     print("\t- content: {}".format(text))
