29339874f53008e90c7135376efe7cabde6a5cd8
root <root@CROBO.localdomain>
2025-06-05 12:10:31 +0200

    done

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -8,13 +8,13 @@ request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error); // Print the error if one occurred
   } else {
-    const jso = JSON.parse(body);
-    const results = jso['results'];
-    const count = 0;
+    let jso = JSON.parse(body);
+    let results = jso['results'];
+    let  count = 0;
     for (let i = 0; i < results.length; i++) {
-      const chars = (results[i]['characters']);
+      let chars = (results[i]['characters']);
       for (let j = 0; j < chars.length; j++) {
-        const check18 = chars[j].endsWith('18/');
+        let check18 = chars[j].endsWith('18/');
         if (check18) {
           count++;
         }
