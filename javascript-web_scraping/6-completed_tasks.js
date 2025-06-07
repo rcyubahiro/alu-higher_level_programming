fdbb8f4715818495286eb4b43bb85ee29e2de063
root <root@CROBO.localdomain>
2025-06-07 19:17:46 +0200

    done

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,6 +4,7 @@
 const args = process.argv;
 const reqURL = args[2];
 const request = require('request');
+
 request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error);
@@ -11,8 +12,8 @@ request(reqURL, function (error, response, body) {
     const todos = JSON.parse(body);
     const dash = {};
     for (let i = 0; i < todos.length; i++) {
-      const status = (todos[i]['completed']);
-      const key = todos[i]['userId'].toString();
+      const status = todos[i].completed;
+      const key = todos[i].userId.toString();
       if (status) {
         if (dash[key]) {
           dash[key]++;
